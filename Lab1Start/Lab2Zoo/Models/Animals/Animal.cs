a8254824e72c60a9b14345df14930ec16c40132d
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-02 00:27:02 +0300

    Added Name, Weight, IsSleeping to Animal. Added CurrentDayMode, GetRandomAnimal() to Zoo and refactored Voice() in Zoo

--- Lab1Start/Lab2Zoo/Models/Animals/Animal.cs
+++ Lab1Start/Lab2Zoo/Models/Animals/Animal.cs
@@ -7,6 +7,37 @@ namespace Lab2Zoo.Models.Animals
 {
     public abstract class Animal : BaseEntity, IComponent
     {
+        private string name = "Unknown";
+        private ushort weight = 0;
+        public bool IsSleeping = false;
+
+        public ushort Weight
+        {
+            get => weight;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Weight should be non-negative");
+                weight = value;
+            }
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("'" + value + "' can not be name");
+                else
+                    name = value;
+            }
+        }  
+
+        public void WakeUp()
+        {
+            IsSleeping = false;
+        }
+
         public abstract string Voice();
         public void Add(IComponent component)
         {
--- Lab1Start/Lab2Zoo/Models/Zoo.cs
+++ Lab1Start/Lab2Zoo/Models/Zoo.cs
@@ -3,19 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Lab2Zoo.Models.Enums;
+using Lab2Zoo.Models.Animals;
+
 namespace Lab2Zoo.Models
 {
     public class Zoo : BaseEntity, IComponent
     {
-        public List<IComponent> Components = new List<IComponent>();
+        public DayMode CurrentDayMode = DayMode.Day;
+        public List<IComponent> Components = new List<IComponent>();       
 
+        public Animal GetRandomAnimal()
+        {
+            return Factories.AnimalFactories.AnimalFactory.CreateRandomAnimal();
+        }
+                
         public string Voice()
         {
+            if (Components.Count < 1) return "No one in zoo";
             StringBuilder stringBuilder = new StringBuilder();
-            foreach (var component in Components)
+
+            if(CurrentDayMode == DayMode.Day)
             {
-                stringBuilder.Append(component.Voice());
+                foreach (var component in Components)
+                {
+                    stringBuilder.Append(component.Voice());
+                }                
             }
+            else
+            {
+                foreach (var item in Components)
+                {
+                    if (item is Animal)
+                    {
+                        if((item as Animal).IsSleeping)
+                        {
+                            (item as Animal).WakeUp();                            
+                        }
+                        stringBuilder.Append((item as Animal).Voice());
+                    }
+                    else stringBuilder.Append(item.Voice());
+                }
+            }
+
             return stringBuilder.ToString();
         }
 
