5017712b0d7ebedfaa42cf0bb093595331164a09
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-12 17:18:26 +0300

    Added Facts to test work with random age/ wrong age

--- Lab1Start/ZooXUnitTestProject/TestClasses/FactoriesTestClasses/AnimalFactoriesTestClasses/GiraffeFactoryTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/FactoriesTestClasses/AnimalFactoriesTestClasses/GiraffeFactoryTestClass.cs
@@ -8,10 +8,40 @@ namespace ZooXUnitTestProject.TestClasses.FactoriesTestClasses.AnimalFactoriesTe
 {
     public class GiraffeFactoryTestClass
     {
+        readonly GiraffeFactory giraffeFactory;
+
+        public GiraffeFactoryTestClass()
+        {
+            giraffeFactory = new GiraffeFactory();
+        }
+
         [Fact]
         public void CreateAnimal_WhenCreatAnimal_ReturnsTrue()
         {
-            Assert.IsType<GiraffeFactory>(new WhiteWolfFactory().CreateAnimal());
+            Assert.IsType<Giraffe>(giraffeFactory.CreateAnimal());
+        }
+
+        [Theory]        
+        [InlineData(5)]
+        [InlineData(25)]
+        public void CreateAnimal_WhenSetAge_ReturnsTrue(byte age)
+        {
+            Assert.Equal(age, (giraffeFactory.CreateAnimal(age) as Giraffe).Age);
+        }
+
+        [Fact]
+        public void CreateAnimal_WhenSetWrognAge_ReturnsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => giraffeFactory.CreateAnimal(0));
+        }
+
+        [Fact]
+        public void CreateAnimal_WhenCreate_ReturnsTrue()
+        {
+            Giraffe giraffe = giraffeFactory.CreateAnimal() as Giraffe;
+
+            Assert.True(giraffe.Age < 31);
+            Assert.True(giraffe.Age > 0);
         }
     }
 }
