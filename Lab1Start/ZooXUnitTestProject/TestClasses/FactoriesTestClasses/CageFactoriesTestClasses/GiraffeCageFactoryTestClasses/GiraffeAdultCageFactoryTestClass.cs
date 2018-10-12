1787211d12c9c38b71be28d655c52a4722666421
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-12 19:26:24 +0300

    Added 2 Facts

--- Lab1Start/ZooXUnitTestProject/TestClasses/FactoriesTestClasses/CageFactoriesTestClasses/GiraffeCageFactoryTestClasses/GiraffeAdultCageFactoryTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/FactoriesTestClasses/CageFactoriesTestClasses/GiraffeCageFactoryTestClasses/GiraffeAdultCageFactoryTestClass.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using Xunit;
 
-using Lab2Zoo.Models.Animals;
-using Lab2Zoo.Models.Factories.AnimalFactories;
+using Lab2Zoo.Models.Cages;
+using Lab2Zoo.Models.Cages.GiraffeCages;
+using Lab2Zoo.Models.Factories.CageFactories.GiraffeCageFactory;
 
 namespace ZooXUnitTestProject.TestClasses.FactoriesTestClasses.CageFactoriesTestClasses.GiraffeCageFactoryTestClasses
 {
     public class GiraffeAdultCageFactoryTestClass
     {
         [Fact]
-        public void TestMethod1()
+        public void CreateCage_WhenCreate_IsInstanceIsDerivedBearCage_ReturnsTrue()
         {
+            Assert.IsAssignableFrom<GiraffeCage>(new AdultGiraffeCageFactory().CreateCage());
+        }
 
+        [Fact]
+        public void CreateCage_WhenCreate_IsInstanceIsDerivedCage_ReturnsTrue()
+        {
+            Assert.IsAssignableFrom<GiraffeCage>(new AdultGiraffeCageFactory().CreateCage());
         }
     }
 }
--- Lab1Start/ZooXUnitTestProject/TestClasses/FactoriesTestClasses/CageFactoriesTestClasses/GiraffeCageFactoryTestClasses/GiraffeCageFactoryTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/FactoriesTestClasses/CageFactoriesTestClasses/GiraffeCageFactoryTestClasses/GiraffeCageFactoryTestClass.cs
@@ -2,16 +2,24 @@
 using Xunit;
 
 using Lab2Zoo.Models.Animals;
-using Lab2Zoo.Models.Factories.AnimalFactories;
+using Lab2Zoo.Models.Cages;
+using Lab2Zoo.Models.Cages.GiraffeCages;
+using Lab2Zoo.Models.Factories.CageFactories.GiraffeCageFactory;
 
 namespace ZooXUnitTestProject.TestClasses.FactoriesTestClasses.CageFactoriesTestClasses.GiraffeCageFactoryTestClasses
 {
     public class GiraffeCageFactoryTestClass
     {
         [Fact]
-        public void TestMethod1()
+        public void CreateCage_WhenCreate_IsInstanceIsDerivedBearCage_ReturnsTrue()
         {
+            Assert.IsAssignableFrom<GiraffeCage>(new GiraffeCageFactory().CreateCage());
+        }
 
+        [Fact]
+        public void CreateCage_WhenCreate_IsInstanceIsDerivedCage_ReturnsTrue()
+        {
+            Assert.IsAssignableFrom<Cage<Giraffe>>(new GiraffeCageFactory().CreateCage());
         }
     }
 }
