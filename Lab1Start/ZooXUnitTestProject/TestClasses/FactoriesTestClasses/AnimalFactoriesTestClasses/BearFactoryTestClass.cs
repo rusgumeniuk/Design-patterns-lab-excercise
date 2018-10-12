a14c2ee27f26d58e9ebd6452e3249d0b5d02a147
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-12 17:26:04 +0300

    Added Fact to test Male after setting

--- Lab1Start/ZooXUnitTestProject/TestClasses/FactoriesTestClasses/AnimalFactoriesTestClasses/BearFactoryTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/FactoriesTestClasses/AnimalFactoriesTestClasses/BearFactoryTestClass.cs
@@ -3,15 +3,29 @@ using Xunit;
 
 using Lab2Zoo.Models.Animals;
 using Lab2Zoo.Models.Factories.AnimalFactories;
+using Lab2Zoo.Models.Enums;
 
 namespace ZooXUnitTestProject.TestClasses.FactoriesTestClasses.AnimalFactoriesTestClasses
 {
     public class BearFactoryTestClass
     {
+        readonly BearFactory bearFactory;
+
+        public BearFactoryTestClass()
+        {
+            bearFactory = new BearFactory();
+        }
+
         [Fact]
         public void CreateAnimal_WhenCreatAnimal_ReturnsTrue()
         {
-            Assert.IsType<Bear>(new WhiteWolfFactory().CreateAnimal());
+            Assert.IsType<Bear>(bearFactory.CreateAnimal());
+        }        
+
+        [Fact]
+        public void CreateAnimal_WhenSetMale_ReturnsTrue()
+        {
+            Assert.Equal(MaleMode.Male, (bearFactory.CreateAnimal(MaleMode.Male) as Bear).Male);
         }
     }
 }
