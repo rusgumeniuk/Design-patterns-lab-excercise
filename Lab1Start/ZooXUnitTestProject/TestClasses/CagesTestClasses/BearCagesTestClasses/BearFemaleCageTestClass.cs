e750b6aed227b5d37e5b75fade20665ca47814ad
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-12 20:26:05 +0300

    Added 2 facts Add

--- Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/BearCagesTestClasses/BearFemaleCageTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/BearCagesTestClasses/BearFemaleCageTestClass.cs
@@ -3,15 +3,41 @@ using Xunit;
 
 using Lab2Zoo.Models.Animals;
 using Lab2Zoo.Models.Factories.AnimalFactories;
+using Lab2Zoo.Models.Factories.CageFactories.BearCageFactory;
+using Lab2Zoo.Models.Cages.BearCages;
 
 namespace ZooXUnitTestProject.TestClasses.CagesTestClasses.BearCagesTestClasses
 {
     public class BearFemaleCageTestClass
     {
-        [Fact]
-        public void TestMethod1()
-        {
+        readonly BearFemaleCage cage;
+        readonly Bear bear;
 
+        public BearFemaleCageTestClass()
+        {
+            cage = new FemaleBearCageFactory().CreateCage() as BearFemaleCage;
+            bear = new BearFactory().CreateAnimal() as Bear;
+        }
+
+        [Fact]
+        public void Add_WhenAddFemaleBear_ReturnsTrue()
+        {
+            bear.Male = Lab2Zoo.Models.Enums.MaleMode.Female;
+            cage.Add(bear);
+
+            Assert.NotEmpty(cage.Components);
+            Assert.NotEmpty(cage.GetAnimals());
+            Assert.Equal(bear, cage.Components[0]);
+        }
+
+        [Fact]
+        public void Add_WhenAddMaleBear_ReturnsTrue()
+        {
+            bear.Male = Lab2Zoo.Models.Enums.MaleMode.Male;
+            cage.Add(bear);
+
+            Assert.Empty(cage.Components);
+            Assert.Empty(cage.GetAnimals());
         }
     }
 }
