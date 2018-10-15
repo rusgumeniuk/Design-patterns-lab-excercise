a0520efe30cdcc11302d64a1ce5f7e5e2914b11c
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-15 22:25:29 +0300

    Added test methods IsContainerCanContainsContainer(), IsContainerCanContainsAnimal()

--- Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/GiraffeCagesTestClass/GiraffeAdultCageTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/GiraffeCagesTestClass/GiraffeAdultCageTestClass.cs
@@ -3,15 +3,52 @@ using Xunit;
 
 using Lab2Zoo.Models.Animals;
 using Lab2Zoo.Models.Factories.AnimalFactories;
+using Lab2Zoo.Models.Factories.CageFactories.GiraffeCageFactory;
+using Lab2Zoo.Models.Cages.GiraffeCages;
 
 namespace ZooXUnitTestProject.TestClasses.CagesTestClasses.GiraffeCagesTestClasses
 {
     public class GiraffeAdultCageTestClass
     {
-        [Fact]
-        public void TestMethod1()
-        {
+        readonly AdultGiraffeCageFactory adultGiraffeCageFactory;
+        readonly ChildrenGiraffeCageFactory childrenGiraffeCageFactory;
 
+        readonly GiraffeAdultCage giraffeAdultCage;
+        readonly GiraffeChildrenCage giraffeChildrenCage;
+
+        readonly GiraffeFactory giraffeFactory;
+        readonly Giraffe giraffe;
+
+        public GiraffeAdultCageTestClass()
+        {
+            adultGiraffeCageFactory = new AdultGiraffeCageFactory();
+            childrenGiraffeCageFactory = new ChildrenGiraffeCageFactory();
+            giraffeFactory = new GiraffeFactory();
+
+            giraffeAdultCage = adultGiraffeCageFactory.CreateCage() as GiraffeAdultCage;
+            giraffeChildrenCage = childrenGiraffeCageFactory.CreateCage() as GiraffeChildrenCage;
+
+            giraffe = giraffeFactory.CreateAnimal() as Giraffe;
+        }
+
+        [Fact]
+        public void IsContainerCanContainsContainer_WhenCanNotContains_ReturnFalse()
+        {
+            Assert.False(giraffeAdultCage.IsContainerCanContainsContainer(giraffeChildrenCage)); 
+        }
+
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenAdult_ReturnTrue()
+        {
+            giraffe.Age = 15;
+            Assert.True(giraffeAdultCage.IsContainerCanContainsAnimal(giraffe));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenChild_ReturnFalse()
+        {
+            giraffe.Age = 13;
+            Assert.False(giraffeAdultCage.IsContainerCanContainsAnimal(giraffe));
         }
     }
 }
--- Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/GiraffeCagesTestClass/GiraffeChildrenCageTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/GiraffeCagesTestClass/GiraffeChildrenCageTestClass.cs
@@ -3,15 +3,52 @@ using Xunit;
 
 using Lab2Zoo.Models.Animals;
 using Lab2Zoo.Models.Factories.AnimalFactories;
+using Lab2Zoo.Models.Factories.CageFactories.GiraffeCageFactory;
+using Lab2Zoo.Models.Cages.GiraffeCages;
 
 namespace ZooXUnitTestProject.TestClasses.CagesTestClasses.GiraffeCagesTestClasses
 {
     public class GiraffeChildrenCageTestClass
     {
-        [Fact]
-        public void TestMethod1()
-        {
+        readonly AdultGiraffeCageFactory adultGiraffeCageFactory;
+        readonly ChildrenGiraffeCageFactory childrenGiraffeCageFactory;
 
+        readonly GiraffeAdultCage giraffeAdultCage;
+        readonly GiraffeChildrenCage giraffeChildrenCage;
+
+        readonly GiraffeFactory giraffeFactory;
+        readonly Giraffe giraffe;
+
+        public GiraffeChildrenCageTestClass()
+        {
+            adultGiraffeCageFactory = new AdultGiraffeCageFactory();
+            childrenGiraffeCageFactory = new ChildrenGiraffeCageFactory();
+            giraffeFactory = new GiraffeFactory();
+
+            giraffeAdultCage = adultGiraffeCageFactory.CreateCage() as GiraffeAdultCage;
+            giraffeChildrenCage = childrenGiraffeCageFactory.CreateCage() as GiraffeChildrenCage;
+
+            giraffe = giraffeFactory.CreateAnimal() as Giraffe;
+        }
+
+        [Fact]
+        public void IsContainerCanContainsContainer_WhenCanNotContains_ReturnFalse()
+        {
+            Assert.False(giraffeChildrenCage.IsContainerCanContainsContainer(giraffeAdultCage));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenAdult_ReturnFalse()
+        {
+            giraffe.Age = 15;
+            Assert.False(giraffeChildrenCage.IsContainerCanContainsAnimal(giraffe));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenChild_ReturnTrue()
+        {
+            giraffe.Age = 13;
+            Assert.True(giraffeChildrenCage.IsContainerCanContainsAnimal(giraffe));
         }
     }
 }
