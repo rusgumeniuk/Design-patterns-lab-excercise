4dad62ee7b4f7a990aeb562efe62c89f99085441
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-15 16:55:09 +0300

    Added tests for Name/Weight/Add()/Remove()/GetAnimals()/GetWeight()

--- Lab1Start/ZooXUnitTestProject/TestClasses/AnimalTestClasses/AnimalTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/AnimalTestClasses/AnimalTestClass.cs
@@ -8,10 +8,91 @@ namespace ZooXUnitTestProject.TestClasses.AnimalTestClasses
 {
     public class AnimalTestClass
     {
-        [Fact]
-        public void Name_WhenSetNull_ReturnsArgumentException()
+        readonly GreyWolfFactory greyWolfFactory;
+        readonly GreyWolf greyWolf;
+
+        public AnimalTestClass()
         {
-            Assert.Throws<ArgumentException>(() => AnimalFactory.CreateRandomAnimal().Name = null);
+            greyWolfFactory = new GreyWolfFactory();
+            greyWolf = greyWolfFactory.CreateAnimal() as GreyWolf;
+        }
+
+        #region Name       
+        [Fact]
+        public void Name_WhenChangeNameOnNull_ReturnsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => greyWolf.Name = null);
+        }
+
+        [Fact]
+        public void Name_WhenChangeNameOnEmptyString_ReturnsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => greyWolf.Name = String.Empty);
+        }
+
+        [Fact]
+        public void Name_WhenSetNullNameInCreatingProccess_ReturnsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => greyWolfFactory.CreateAnimal().Name = null);
+        }
+
+        [Fact]
+        public void Name_WhenSetRealString_ReturnsTrue()
+        {
+            string newName = "NameTest";
+            greyWolf.Name = newName;
+            Assert.Equal(newName, greyWolf.Name);
+        }
+        #endregion
+
+        #region Weight
+        [Fact]
+        public void Weight_WhenSetZeroWeight_ReturnsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => greyWolf.Weight = 0);
+        }
+
+        [Fact]
+        public void Weight_WhenSet50Weight_ReturnsTrue()
+        {
+            int weight = 50;
+            ushort convertedWeight = Convert.ToUInt16(weight);
+            greyWolf.Weight = convertedWeight;
+            Assert.Equal(weight, greyWolf.Weight);
+        }
+        #endregion
+
+        [Fact]
+        public void Add_ReturnsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => greyWolf.Add(greyWolf));
+        }
+
+        [Fact]
+        public void Remove_ReturnsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => greyWolf.Remove(greyWolf));
+        }
+
+        [Fact]
+        public void GetAnimals_ReturnsListWithItself()
+        {
+            var result = greyWolf.GetAnimals();
+
+            Assert.Single(result);
+            Assert.Equal(greyWolf, result[0]);
+        }
+
+        [Fact]
+        public void GetAmountOfAnimals_Returns1()
+        {
+            Assert.Equal(1, greyWolf.GetAmountOfAnimals());
+        }
+
+        [Fact]
+        public void GetWeight_ReturnsTrue()
+        {
+            Assert.Equal(greyWolf.Weight, greyWolf.GetWeight());
         }
     }
 }
