1ee70eaab2b2760cca36ed291d8e502071cddaeb
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-15 21:47:06 +0300

    Added 2 test methods Age

--- Lab1Start/ZooXUnitTestProject/TestClasses/AnimalTestClasses/GiraffeTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/AnimalTestClasses/GiraffeTestClass.cs
@@ -8,10 +8,27 @@ namespace ZooXUnitTestProject.TestClasses.AnimalTestClasses
 {
     public class GiraffeTestClass
     {
-        [Fact]
-        public void TestMethod1()
-        {
+        readonly GiraffeFactory giraffeFactory;
+        readonly Giraffe giraffe;
 
+        public GiraffeTestClass()
+        {
+            giraffeFactory = new GiraffeFactory();
+            giraffe = giraffeFactory.CreateAnimal(10) as Giraffe;
         }
+
+        [Fact]
+        public void Age_WhenSet20_ReturnTrue() 
+        {
+            int newAge = 20;
+            giraffe.Age = (byte)newAge;
+            Assert.Equal(newAge, giraffe.Age);
+        }
+
+        [Fact]
+        public void Age_WhenSet0_ReturnArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => giraffe.Age = 0); 
+        }        
     }
 }
