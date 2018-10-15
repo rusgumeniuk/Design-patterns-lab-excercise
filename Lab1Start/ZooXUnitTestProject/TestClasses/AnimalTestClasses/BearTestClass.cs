f176439bd8d8bfbcef6c500100d269966c664bb1
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-15 21:41:58 +0300

    Added test methods Voice() and ChangeMaleMode()

--- Lab1Start/ZooXUnitTestProject/TestClasses/AnimalTestClasses/BearTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/AnimalTestClasses/BearTestClass.cs
@@ -8,10 +8,26 @@ namespace ZooXUnitTestProject.TestClasses.AnimalTestClasses
 {
     public class BearTestClass
     {
-        [Fact]
-        public void TestMethod1()
-        {
+        readonly BearFactory bearFactory;
+        readonly Bear bear;
 
+        public BearTestClass()
+        {
+            bearFactory = new BearFactory();
+            bear = bearFactory.CreateAnimal(Lab2Zoo.Models.Enums.MaleMode.Female) as Bear;
+        }
+
+        [Fact]
+        public void MaleMode_WhenChangeMale_ReturnsTrue()
+        {
+            bear.Male = Lab2Zoo.Models.Enums.MaleMode.Male;
+            Assert.Equal(Lab2Zoo.Models.Enums.MaleMode.Male, bear.Male);
+        }
+
+        [Fact]
+        public void Voice_WhenGetVoice_ReturnsTrue()
+        {
+            Assert.Equal(bear.Id + ": Grrrrr", bear.Voice());
         }
     }
 }
