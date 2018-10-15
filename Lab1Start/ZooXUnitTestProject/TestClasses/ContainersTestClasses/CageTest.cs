73b831bdb8e9442a28b177a08ae534d20cd1fb43
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-15 21:20:46 +0300

    Added test methods IsContainerIsParentContainer() and IsContainerAlreadyContainsComponent()

--- Lab1Start/ZooXUnitTestProject/TestClasses/ContainersTestClasses/CageTest.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/ContainersTestClasses/CageTest.cs
@@ -3,6 +3,8 @@ using Xunit;
 
 using Lab2Zoo.Models.Factories.CageFactories.BearCageFactory;
 using Lab2Zoo.Models.Cages.BearCages;
+using Lab2Zoo.Models.Animals;
+using Lab2Zoo.Models.Factories.AnimalFactories;
 
 namespace ZooXUnitTestProject.TestClasses.ContainersTestClasses
 {
@@ -33,10 +35,47 @@ namespace ZooXUnitTestProject.TestClasses.ContainersTestClasses
             Assert.False(bearFemaleCage.IsContainerCanContainsContainer(bearCage));
         }
 
+
         [Fact]
         public void Voice_WhenEcmptyCage_ReturnsEmptyCage()
         {
             Assert.Equal("Empty cage " + bearCage.Id, bearCage.Voice());
+        }
+
+
+        [Fact]
+        public void IsContainerIsParentContainer_WhenUserTryToAddToContainerOtherContainerWhichIsParentContainer_ReturnsTrue()
+        {
+            bearCage.Add(bearFemaleCage);
+            Assert.True(bearFemaleCage.IsContainerIsParentContainer(bearCage));
+        }
+
+        [Fact]
+        public void IsContainerIsParentContainer_WhenCheckInnerCage_ReturnsFalse()
+        {
+            bearCage.Add(bearFemaleCage);
+            Assert.False(bearCage.IsContainerIsParentContainer(bearFemaleCage));
+        }
+
+
+        [Fact]
+        public void IsContainerAlreadyContainsComponent_WhenTryToAddAlreadyExistingAnimal_ReturnsTrue()
+        {
+            bearCage.Add(bearFemaleCage);
+            Bear bear = new BearFactory().CreateAnimal(Lab2Zoo.Models.Enums.MaleMode.Female) as Bear;
+
+            bearFemaleCage.Add(bear);
+
+            Assert.True(bearCage.IsContainerAlreadyContainsComponent(bear));
+        }
+
+        [Fact]
+        public void IsContainerAlreadyContainsComponent_WhenAnimalNotInAnyContainer_ReturnsFalse()
+        {
+            bearCage.Add(bearFemaleCage);
+            Bear bear = new BearFactory().CreateAnimal(Lab2Zoo.Models.Enums.MaleMode.Female) as Bear;            
+
+            Assert.False(bearCage.IsContainerAlreadyContainsComponent(bear));
         }
     }
 }
