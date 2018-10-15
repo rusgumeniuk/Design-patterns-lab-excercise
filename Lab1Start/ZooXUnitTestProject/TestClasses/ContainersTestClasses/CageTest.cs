a52739b1cf0d4d80a92c6377a636968290987133
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-15 19:51:07 +0300

    Added test methods IsContainerCanContainsContainer()

--- Lab1Start/ZooXUnitTestProject/TestClasses/ContainersTestClasses/CageTest.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/ContainersTestClasses/CageTest.cs
@@ -3,23 +3,34 @@ using Xunit;
 
 using Lab2Zoo.Models.Factories.CageFactories.BearCageFactory;
 using Lab2Zoo.Models.Cages.BearCages;
+
 namespace ZooXUnitTestProject.TestClasses.ContainersTestClasses
 {
     public class CageTest
     {
-        readonly BearCageFactory cageFactory;
+        readonly BearCageFactory bearCageFactory;
+        readonly FemaleBearCageFactory femaleBearCageFactory;
         readonly BearCage bearCage;
+        readonly BearFemaleCage bearFemaleCage;
 
         public CageTest()
         {
-            cageFactory = new BearCageFactory();
-            bearCage = cageFactory.CreateCage() as BearCage;
+            bearCageFactory = new BearCageFactory();
+            femaleBearCageFactory = new FemaleBearCageFactory();
+            bearCage = bearCageFactory.CreateCage() as BearCage;
+            bearFemaleCage = femaleBearCageFactory.CreateCage() as BearFemaleCage;
         }
 
         [Fact]
         public void IsContainerCanContainsAnimal_WhenCanContains_ReturnsTrue()
         {
+            Assert.True(bearCage.IsContainerCanContainsContainer(bearFemaleCage));
+        }
 
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenCanNotContains_ReturnsFalse()
+        {
+            Assert.False(bearFemaleCage.IsContainerCanContainsContainer(bearCage));
         }
 
         [Fact]
