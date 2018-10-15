42e551f095c89f57f9e6ed00998faf6b54bcc1c8
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-15 16:20:56 +0300

    Class for testing extension method Type.GetBaseTypes(). Created 3 tests

---
+++ Lab1Start/ZooXUnitTestProject/TestClasses/AdditionalTypeClassTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+
+using Lab2Zoo.Models;
+
+namespace ZooXUnitTestProject.TestClasses
+{
+    public class AdditionalTypeClassTest
+    {
+        [Fact]
+        public void GetBaseTypes_WhenItemHasNotBaseType_ReturnsNull()
+        {
+            Assert.Null(typeof(object).GetBaseTypes());
+        }
+
+        [Fact]
+        public void GetBaseTypes_WhenItemHas1BaseType_ReturnsTrue()
+        {
+            Assert.Equal(2, typeof(string).GetBaseTypes().Length);
+        }
+
+        [Fact]
+        public void GetBaseTypes_WhenZoo_ReturnsTrue()
+        {   //obj <- component <- container <- zoo
+            Assert.Equal(4, new Zoo().GetType().GetBaseTypes().Length);
+            Assert.Contains(typeof(Component), typeof(Zoo).GetBaseTypes());
+            Assert.Contains(typeof(Zoo), typeof(Zoo).GetBaseTypes());
+        }
+    }
+}
