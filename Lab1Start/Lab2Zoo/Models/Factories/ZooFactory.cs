8724b13b45fbbd0088f7f77157c30a9bd35d863e
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-16 11:48:10 +0300

    Added ZooFactory

---
+++ Lab1Start/Lab2Zoo/Models/Factories/ZooFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Zoo.Models.Factories
+{
+    public class ZooFactory
+    {
+        public static Zoo CreateZoo()
+        {
+            return new Zoo();
+        }        
+    }
+}
