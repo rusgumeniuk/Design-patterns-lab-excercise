3bef432a5a369b1a64bae6449bb52da7e632b322
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-10-16 09:11:25 +0300

    Added test methods ISContainerCanContainsContainer()/IsContainerCanContainsAnimal()

--- Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/WolfCagesTestClass/GreyWolfCageTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/WolfCagesTestClass/GreyWolfCageTestClass.cs
@@ -3,15 +3,51 @@ using Xunit;
 
 using Lab2Zoo.Models.Animals;
 using Lab2Zoo.Models.Factories.AnimalFactories;
+using Lab2Zoo.Models.Factories.CageFactories.WolfCageFactory;
+using Lab2Zoo.Models.Cages.WolfCages;
 
 namespace ZooXUnitTestProject.TestClasses.CagesTestClasses.WolfCagesTestClasses
 {
     public class GreyWolfCageTestClass
     {
-        [Fact]
-        public void TestMethod1()
-        {
+        readonly GreyWolfCage greyWolfCage;
+        readonly WhiteWolfCage whiteWolfCage;
 
+        readonly WhiteWolf whiteWolf;
+        readonly GreyWolf greyWolf;
+
+        public GreyWolfCageTestClass()
+        {
+            greyWolfCage = new GreyWolfCageFactory().CreateCage() as GreyWolfCage;
+            whiteWolfCage = new WhiteWolfCageFactory().CreateCage() as WhiteWolfCage;
+
+            whiteWolf = new WhiteWolfFactory().CreateAnimal() as WhiteWolf;
+            greyWolf = new GreyWolfFactory().CreateAnimal() as GreyWolf;
+        }
+
+
+        [Fact]
+        public void IsContainerCanContainsContainer_WhenInnerContainerIsGreyWolfCage_ReturnsTrue()
+        {
+            Assert.True(greyWolfCage.IsContainerCanContainsContainer(greyWolfCage));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsContainer_WhenInnerContainerIsWhiteWolfCage_ReturnsFalse()
+        {
+            Assert.False(greyWolfCage.IsContainerCanContainsContainer(whiteWolfCage));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenComponentIsGreyWolf_ReturnsTrue()
+        {
+            Assert.True(greyWolfCage.IsContainerCanContainsAnimal(greyWolf));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenComponentIsWhiteWolf_ReturnsFalse()
+        {
+            Assert.False(greyWolfCage.IsContainerCanContainsAnimal(whiteWolf));
         }
     }
 }
--- Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/WolfCagesTestClass/WhiteWolfCageTestClass.cs
+++ Lab1Start/ZooXUnitTestProject/TestClasses/CagesTestClasses/WolfCagesTestClass/WhiteWolfCageTestClass.cs
@@ -3,15 +3,50 @@ using Xunit;
 
 using Lab2Zoo.Models.Animals;
 using Lab2Zoo.Models.Factories.AnimalFactories;
+using Lab2Zoo.Models.Factories.CageFactories.WolfCageFactory;
+using Lab2Zoo.Models.Cages.WolfCages;
 
 namespace ZooXUnitTestProject.TestClasses.CagesTestClasses.WolfCagesTestClasses
 {
     public class WhiteWolfCageTestClass
     {
-        [Fact]
-        public void TestMethod1()
-        {
+        readonly GreyWolfCage greyWolfCage;
+        readonly WhiteWolfCage whiteWolfCage;
 
+        readonly WhiteWolf whiteWolf;
+        readonly GreyWolf greyWolf;
+
+        public WhiteWolfCageTestClass()
+        {
+            greyWolfCage = new GreyWolfCageFactory().CreateCage() as GreyWolfCage;
+            whiteWolfCage = new WhiteWolfCageFactory().CreateCage() as WhiteWolfCage;            
+
+            whiteWolf = new WhiteWolfFactory().CreateAnimal() as WhiteWolf;
+            greyWolf = new GreyWolfFactory().CreateAnimal() as GreyWolf;
+        }
+
+        [Fact]
+        public void IsContainerCanContainsContainer_WhenInnerContainerIsGreyWolfCage_ReturnsFalse()
+        {
+            Assert.False(whiteWolfCage.IsContainerCanContainsContainer(greyWolfCage));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsContainer_WhenInnerContainerIsWhiteWolfCage_ReturnsTrue()
+        {
+            Assert.True(whiteWolfCage.IsContainerCanContainsContainer(whiteWolfCage));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenComponentIsGreyWolf_ReturnsFalse()
+        {
+            Assert.False(whiteWolfCage.IsContainerCanContainsAnimal(greyWolf));
+        }
+
+        [Fact]
+        public void IsContainerCanContainsAnimal_WhenComponentIsWhiteWolf_ReturnsTrue()
+        {
+            Assert.True(whiteWolfCage.IsContainerCanContainsAnimal(whiteWolf));
         }
     }
 }
